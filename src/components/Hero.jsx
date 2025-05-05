b21b118181ad717006a2288edd81d810754ad6c3
harsh rawat <42332761+iharshr@users.noreply.github.com>
2025-05-05 22:59:05 +0530

    updated banner text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,10 @@ export const Hero = () => {
 					{/* text start */}
 					<div className="relative flex flex-col items-center p-4 sm:max-w-xl">
 						<p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">
-							Very proud to introduce
+							Acts of Kindness Start Here
 						</p>
-						<h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">
-							Revolutionary way to build the web
+						<h1 className="mb-8 text-center text-4xl font-bold text-white  md:mb-12 ">
+							Donate. Empower. Change.
 						</h1>
 						<div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
 							<a
