4f0d62f89d826abf55200030602b80375242be82
harsh rawat <42332761+iharshr@users.noreply.github.com>
2025-05-06 00:02:21 +0530

    fix: logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,16 +33,14 @@ export const Footer = () => {
 									className="inline-flex items-center gap-2 text-xl font-bold text-black md:text-2xl"
 									aria-label="logo"
 								>
-									<svg
+									<img
+										src="/logo.png"
+										loading="lazy"
 										width={95}
 										height={94}
-										viewBox="0 0 95 94"
-										className="h-auto w-5 text-indigo-500"
-										fill="currentColor"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path d="M96 0V47L48 94H0V47L48 0H96Z" />
-									</svg>
+										alt="Photo by Radu Florin"
+										className="h-full w-full object-cover object-center"
+									/>
 									CharityBridge
 								</a>
 							</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,16 +9,14 @@ export const Header = () => {
 						className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
 						aria-label="logo"
 					>
-						<svg
+						<img
+							src="/logo.png"
+							loading="lazy"
 							width={95}
 							height={94}
-							viewBox="0 0 95 94"
-							className="h-auto w-6 text-indigo-500"
-							fill="currentColor"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path d="M96 0V47L48 94H0V47L48 0H96Z" />
-						</svg>
+							alt="Photo by Radu Florin"
+							className="h-full w-full object-cover object-center"
+						/>
 						CharityBridge
 					</a>
 					{/* logo - end */}
